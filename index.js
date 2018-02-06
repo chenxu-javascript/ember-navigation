f0e48dfd7c7805330d9d53f2e1288f4a623f88c1
chenxu <chenxu@zbj.com>
2018-02-06 18:16:08 +0800

    dist

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-navigation'
+  name: 'ember-navigation',
+  included: function(app) {
+
+    this._super.included.apply(this, arguments);
+
+    while (typeof app.import !== 'function' && app.app) {
+      app = app.app;
+    }
+    app.import('vendor/styles/app.css');
+
+    this.appConfig = app.project.config(app.env);
+    this.addonConfig = this.appConfig['ember-navigation'] || {};
+
+    return app;
+  },
 };
