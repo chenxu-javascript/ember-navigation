67953735b02ffedab3e27fb3749e885079dd5a38
chenxu <chenxu@zbj.com>
2018-02-07 10:46:17 +0800

    fix

--- blueprints/ember-navigation/index.js
+++ blueprints/ember-navigation/index.js
@@ -2,11 +2,9 @@
 module.exports = {
   normalizeEntityName: function() {},
 
-  afterInstall(options) {
-    if (!options.dryRun && isPackageMissing(this, 'ember-wormhole')) {
-      return this.addPackagesToProject([
-        { name: 'ember-wormhole', target: '^0.5.4' },
-      ]);
-    }
+  afterInstall() {
+    return this.addPackagesToProject([
+      { name: 'ember-wormhole', target: '^0.5.4' },
+    ]);
   }
 };
